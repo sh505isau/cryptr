2762ab4cd0df47e334ed5701f33e40757d176143
Lee <sh505isau@users.noreply.github.com>
2017-11-02 00:28:30 -0700

    major update to use new key and IV

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-var crypto = require('crypto');
+const crypto = require('crypto');
 
-function Cryptr(secret, algorithm, iv){
-	if(!secret || typeof secret !== 'string'){
-		secret = 'defaultSecret';
-		throw new Error('Cryptr: secret must be a non-0-length string');
+function Cryptr(salt, iv, algorithm){
+
+	if(!salt || typeof salt !== 'string'){
+		salt = 'defaultkey';
+		throw new Error('Cryptr: key must be a non-0-length string');
+	}
+
+	if(!iv || typeof iv !== 'string'){
+		iv = 'defaultiv';
+		throw new Error('Cryptr: IV must be a non-0-length string');
 	}
 
 	algorithm = algorithm || 'aes-256-ctr';
-	iv = iv || 'x79APSpNJ52%2FhNemDssdHmgHu8iWYPjCTKUrJeI0zrGs1';
 
 	if(typeof algorithm !== 'string'){
 		throw new Error('Cryptr: algorithm must be a string, see https://nodejs.org/api/crypto.html for details');
 	}
+
+	let key_hash = crypto.createHash('sha256');
+	let iv_hash = crypto.createHash('md5');
+
+	key = key_hash.update(salt).digest().slice(0, 32);
+	iv = iv_hash.update(iv).digest().slice(0, 16);
 
 	this.encrypt = function encrypt(value){
 		if(value == null){
@@ -20,7 +31,7 @@ function Cryptr(secret, algorithm, iv){
 
 		value = String(value);
 
-		var cipher = crypto.createCipheriv(algorithm, secret, iv);
+		let cipher = crypto.createCipheriv(algorithm, key, iv);
 		return cipher.update(value, 'utf8', 'hex') + cipher.final('hex');
 	};
 
@@ -31,7 +42,7 @@ function Cryptr(secret, algorithm, iv){
 
 		value = String(value);
 
-		var decipher = crypto.createDecipheriv(algorithm, secret, iv);
+		let decipher = crypto.createDecipheriv(algorithm, key, iv);
 		return decipher.update(value, 'hex', 'utf8') + decipher.final('utf8');
 	};
 }

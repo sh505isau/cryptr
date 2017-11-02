d601ad410e13b51381086a1d6208d081d822bb32
Lee <sh505isau@users.noreply.github.com>
2017-11-01 23:19:54 -0700

    update to use initialization vectors for crypto

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var crypto = require('crypto');
 
-function Cryptr(secret, algorithm){
+function Cryptr(secret, algorithm, iv){
 	if(!secret || typeof secret !== 'string'){
 		secret = 'defaultSecret';
 		throw new Error('Cryptr: secret must be a non-0-length string');
 	}
 
 	algorithm = algorithm || 'aes-256-ctr';
+	iv = iv || 'x79APSpNJ52%2FhNemDssdHmgHu8iWYPjCTKUrJeI0zrGs1';
 
 	if(typeof algorithm !== 'string'){
 		throw new Error('Cryptr: algorithm must be a string, see https://nodejs.org/api/crypto.html for details');
@@ -19,7 +20,7 @@ function Cryptr(secret, algorithm){
 
 		value = String(value);
 
-		var cipher = crypto.createCipher(algorithm, secret);
+		var cipher = crypto.createCipheriv(algorithm, secret, iv);
 		return cipher.update(value, 'utf8', 'hex') + cipher.final('hex');
 	};
 
@@ -30,7 +31,7 @@ function Cryptr(secret, algorithm){
 
 		value = String(value);
 
-		var decipher = crypto.createDecipher(algorithm, secret);
+		var decipher = crypto.createDecipheriv(algorithm, secret, iv);
 		return decipher.update(value, 'hex', 'utf8') + decipher.final('utf8');
 	};
 }
